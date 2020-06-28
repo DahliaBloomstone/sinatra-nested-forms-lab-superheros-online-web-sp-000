b98e6446cd84572b6dbb07ebad324ce239b23c55
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-06-28 03:54:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
-    set :views, Proc.new { File.join(root, "../views/") }
+set :views, Proc.new { File.join(root, "../views/") }
 
-    get '/' do
-         erb :super_hero
-       end
+  get '/' do
+     erb :super_hero
+   end
 
 
        post '/teams' do
